27eedaabc05cdada574ac1ee997a7793413abc2f
Kirill <kirSM2010@gmail.com>
2022-12-11 18:33:14 +0300

    Added raw algoritm

--- src/high_bit_search.v
+++ src/high_bit_search.v
@@ -1,8 +1,35 @@
 module high_bit_search(
     input clk,
-    input signed [INPUT_WIDTH-1:0] input_data,
+    input [INPUT_WIDTH-1:0] input_data,
 
-    output reg signed [RESULT_WIDTH-1:0]result = 0
+    output reg [RESULT_WIDTH-1:0]result = 0
 );
+
+parameter INPUT_WIDTH = 8;
+localparam RESULT_WIDTH = $clog2(INPUT_WIDTH);
+
+reg [INPUT_WIDTH-1:0] data = 1;
+reg [(INPUT_WIDTH/2)-1:0] half = 0;
+reg [RESULT_WIDTH:0] size = 0;
+
+always @(posedge clk)
+begin
+    data <= input_data;
+    size <= (INPUT_WIDTH / 8) * 4;
+    while (data != 1)
+    begin
+        half <= data >> size;
+        if (half)
+        begin
+            data <= half;
+            result <= result + size;
+        end
+        else
+        begin
+            data <= data ^ (half << size);
+        end
+        size <= size >> 1; 
+    end
+end
 
 endmodule
--- tb/testbench.v
+++ tb/testbench.v
@@ -1,7 +1,34 @@
 module tb;
     reg tb_clk = 1;
+    reg [7:0] data;
+
     initial
         forever #5 tb_clk=~tb_clk;
+
+    localparam TB_INPUT_WIDTH = 8;
+
+    integer i;
+    integer tb_data[9:0];
+
+    initial 
+    begin
+        tb_data[0] = 8'hDE;
+        tb_data[1] = 8'hAD;
+        tb_data[2] = 8'hBE;
+        tb_data[3] = 8'hEF;
+        tb_data[4] = 8'hCA;
+        tb_data[5] = 8'hFE;
+        tb_data[6] = 8'hBA;
+        tb_data[7] = 8'hBA;
+        tb_data[8] = 8'hDE;
+        tb_data[9] = 8'hDA;
+        data = 0;
+        for(i = 0; i < 10; i = i + 1)
+        begin
+            data = tb_data[i];
+            #10;
+        end
+    end
 
     initial
     begin
@@ -9,5 +36,11 @@ module tb;
         $dumpvars(0, tb);
     end
 
-    
+    high_bit_search #(
+        .INPUT_WIDTH(TB_INPUT_WIDTH)
+    ) high_bit_search_instance (
+        .clk(tb_clk),
+        .input_data(data)
+    );
+
 endmodule
